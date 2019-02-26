7159fb188155afb32a3cc05e3680e789df17b2db
Mohammad Asif Hashmi <udolf15@gmail.com>
2019-02-26 13:07:11 +0530

    signup and login pages initial phase

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask('flaskwp1')
 def webprint():
     return render_template('indexTweek2.html') 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 3000)
