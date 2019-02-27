f4fd8d5ab2a3c75c09275f186b15798526546a22
Mohammad Asif Hashmi <udolf15@gmail.com>
2019-02-28 01:49:22 +0530

    basic login and signup facility with database created

--- app.py
+++ app.py
@@ -1,18 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
+from flaskext.mysql import MySQL
 
-app = Flask('flaskwp1')
+app = Flask(__name__)
+
+# configuring DB
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'recommendmemovies'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql = MySQL()
+mysql.init_app(app)
+
+
 # webcode = open('webcode.html').read() - not needed
 
 @app.route('/')
 def webprint():
-    return render_template('indexTweek2.html') 
+    return render_template('index.html') 
 
-@app.route('/login')
+@app.route('/login' , methods= ['GET','POST'])
 def login():
+    if(request.method== 'POST'):
+        userDetail = request.form
+        userName = userDetail['username']
+        userPassword = userDetail['password']
+
+        conn = mysql.connect()
+        curr = conn.cursor()
+        curr.execute("SELECT * FROM users WHERE user_name = %s AND password = %s ",(userName,userPassword))
+        data = curr.fetchone()
+        if(data == None):
+            return 'Error bitch'
+        else:
+            return render_template('loggedin.html',userName = userName)
+
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods = ['GET','POST'])
 def signup():
+    if(request.method == 'POST'):
+        userDetails = request.form
+        userName = userDetails['username']
+        userPassword = userDetails['password']
+        userEmail = userDetails['email']
+        conn = mysql.connect()
+        curr = conn.cursor()
+        curr.execute("INSERT INTO users (user_name , email, password) VALUES(%s,%s,%s)",(userName,userEmail,userPassword))
+        conn.commit()
+        curr.close()
+        return 'success dude'
     return render_template('signup.html')
 
 if __name__ == '__main__':
