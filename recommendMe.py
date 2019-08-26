dab4c28e1650b42a84d122bad43220ae39d90337
Mohammad Asif Hashmi <hashmi.masif@gmail.com>
2019-08-26 07:52:05 +0530

    Update recommendMe.py

--- recommendMe.py
+++ recommendMe.py
@@ -4,9 +4,7 @@ from scipy import stats
 from ast import literal_eval
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import wordnet
+
 from surprise import Reader, Dataset, SVD, evaluate
 from imdbToId import converter
 
